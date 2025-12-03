86739350e476549c5b9bffb9ad5645b68489d536
KeiraVenic <karrelvpc13@gmail.com>
2025-12-03 15:33:35 +0700

    update untuk link stylesheet kasir

--- view/kasir/kasirPage.php
+++ view/kasir/kasirPage.php
@@ -10,7 +10,8 @@ if (!isset($data)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kasir Page - Hotel</title>
-    <!-- <link rel="stylesheet" href="../../asset/css/kasir.css"> -->
+    <link rel="stylesheet" href="../../asset/css/kasir.css">
+    
 </head>
 <body>
     <sidebar>
--- view/kasir/otsOrder.php
+++ view/kasir/otsOrder.php
@@ -10,7 +10,7 @@ if (!isset($data)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OTS Order - Hotel</title>
-    <!-- <link rel="stylesheet" href="../../asset/css/kasir.css"> -->
+    <link rel="stylesheet" href="../../asset/css/kasir.css">
 </head>
 <body>
     <sidebar>
