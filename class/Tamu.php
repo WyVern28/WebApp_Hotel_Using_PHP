5a127615a1cea83194cc9deec2e4102da0de64e0
William Prasetyo Utomo <williamellio05@gmail.com>
2025-12-09 23:17:41 +0700

    update tamu.php

--- class/Tamu.php
+++ class/Tamu.php
@@ -29,8 +29,9 @@ class Tamu extends Database {
     }
     public function updateProfile($username,$data){
         try{
-            $query = $this->db->prepare("UPDATE tamu SET nama_lengkap = :nama_lengkap, no_hp = :no_hp WHERE username = :username");
+            $query = $this->db->prepare("UPDATE tamu SET nama_lengkap = :nama_lengkap,no_ktp = :no_ktp, no_hp = :no_hp WHERE username = :username");
             $query ->bindParam(":nama_lengkap", $data['nama_lengkap']);
+            $query ->bindParam(":no_ktp", $data['no_ktp']);
             $query ->bindParam(":no_hp", $data['no_hp']);
             $query ->bindParam(":username", $username);
             return $query->execute();
