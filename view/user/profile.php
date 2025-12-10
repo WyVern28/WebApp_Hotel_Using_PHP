a649686c3f0208966f6e44d3bfee13dec3d1b68a
KeiraVenic <karrelvpc13@gmail.com>
2025-12-10 13:28:07 +0700

    update profile

--- view/user/profile.php
+++ view/user/profile.php
@@ -11,8 +11,9 @@ if (!isset($data)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile - Hotel</title>
-    <link rel="stylesheet" href="../../asset/css/profile.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../asset/css/profile.css">
 </head>
 
 <body>
